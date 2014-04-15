9db51fdd617655d5cdf1f7091f8f50e796801d97
omardelarosa <thedelarosa@gmail.com>
2014-04-15 13:36:54 -0400

    minor changes

--- install.sh
+++ install.sh
@@ -383,4 +383,4 @@ figlet_announces "fin"
 echo "We're done"
 echo "You may want to quit terminal. Reopen and then run brew doctor to ensure"
 echo "everything is working."
-# - fin - #
+# -- fin -- #
