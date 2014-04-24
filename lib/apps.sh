0dbbb41aa3760ef05214810b8babdb297c76fbd3
phlco <phillip.lamplugh@gmail.com>
2014-04-24 10:36:35 -0400

    updates builder

---
+++ lib/apps.sh
@@ -0,0 +1,16 @@
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+brew cask install dash
+
+brew cask install firefox
+
+brew cask install google-chrome
+
+brew cask install hipchat
+
+brew cask install spectacle
+
+brew cask install sublime-text
+
+brew cask install xquartz
