44c0e327e58fcc8562b9072accd5ada0d75091a7
phlco <phillip.lamplugh@gmail.com>
2014-04-27 13:23:27 -0400

    adds yes to rvm script

--- lib/remove_rvm.sh
+++ lib/remove_rvm.sh
@@ -4,7 +4,7 @@
 # Uninstall RVM
 # http://stackoverflow.com/questions/3950260/howto-uninstall-rvm
 if hash rvm 2>/dev/null || [ -d ~/.rvm ]; then
-  rvm implode
+  yes | rvm implode
   rm -rf ~/.rvm
   echo "RVM has been removed."
 else
