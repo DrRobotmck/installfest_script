839c0494d258f1a3d01cfc1c6aea54d3533e1690
phlco <phillip.lamplugh@gmail.com>
2014-04-27 19:38:54 -0400

    removes .ruby version file, changes source to lib'

--- lib/dramatis_personae.sh
+++ lib/dramatis_personae.sh
@@ -1,2 +1,6 @@
 MINIMUM_OS="10.7.0"
 BELOVED_RUBY_VERSION="1.9.3-p484"
+
+SRC_DIR=~/.wdi-installfest
+SETTINGS=$SRC_DIR/settings
+INSTALL_REPO=https://github.com/ga-instructors/installfest_script.git
