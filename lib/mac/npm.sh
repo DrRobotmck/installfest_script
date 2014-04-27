dbb49fc7db7353f5f60c6d2c7fd0016db41e7a1e
phlco <phillip.lamplugh@gmail.com>
2014-04-27 19:11:11 -0400

    update

--- lib/mac/npm.sh
+++ lib/mac/npm.sh
@@ -1,1 +1,7 @@
-npm.sh
+npm install -g yo
+npm install -g gulp
+npm install -g grunt-cli
+npm install -g jshint
+npm install -g browserify
+npm install -g generator-webapp
+npm install -g generator-gulp-webapp
