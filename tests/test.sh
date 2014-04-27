14d4484bc355b722d0effa535876dcccfbc87253
phlco <phillip.lamplugh@gmail.com>
2014-04-27 19:12:43 -0400

    puts test script bck together

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,55 @@
 ##Checkpoints to see if you computer is configured properly.
-source ./spec_helper.sh
+aye () {
+  msg=$1
+  GREEN=$(tput setaf 2)
+  RESET=$(tput sgr0)
+  echo "${GREEN}$msg${RESET}"
+}
+
+nay () {
+  msg=$1
+  RED=$(tput setaf 1)
+  RESET=$(tput sgr0)
+  echo "${RED}$msg${RESET}"
+  ((++checkpoints));
+}
+
+# Instruments of Darkness
+what_news_of () {
+  app_name=$1
+  echo -n "What news of $app_name? "
+  # This could also work
+  # if ls /path/to/your/files* &> /dev/null;
+  # add support for Application folder or brew cask
+  if [ -x /Applications/"$app_name".app ] || [ -x /opt/homebrew-cask/Caskroom/*/*/"$app_name".app ]; then
+    aye "Good"
+  else
+    nay "Alack!"
+    ((++checkpoints));
+  fi
+}
+
+dost_thou_have () {
+  filename=$1
+  # That which thou seeks is thine
+  if [ -e "$filename" ]; then
+    aye "Good on $filename"
+  else
+    nay "Thou searchest in vain for $filename"
+    ((++checkpoints));
+  fi
+}
+
+is_not_this () {
+  cmd=$($1)
+  pass=$2
+  if [[ $cmd == *"$pass"* ]]; then
+    aye "$1 is $pass"
+  else
+    nay "Alack! $1 is not $pass"
+    ((++checkpoints));
+  fi
+}
 
 # Original checkpoints script from Peter Lai.
 
