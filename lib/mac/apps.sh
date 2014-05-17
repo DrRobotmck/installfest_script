7a8049386e91a275ea955acc560a0ce771980c46
phlco <phillip.lamplugh@gmail.com>
2014-05-17 10:28:00 -0400

    adds flux

--- lib/mac/apps.sh
+++ lib/mac/apps.sh
@@ -29,6 +29,9 @@ brew cask install xquartz
 # Markdown Editor
 brew cask install mou
 
+# Flux, makes the color of your computer's display adapt to the time of day
+brew cask install flux
+
 # for fonts
 # https://github.com/caskroom/homebrew-fonts/tree/master/Casks
 brew tap caskroom/fonts
