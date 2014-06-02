13654e5135865abc5afcb5481e061197617865ca
phlco <phillip.lamplugh@gmail.com>
2014-06-02 18:56:20 -0400

    adds sudo to apt-get commands

--- scripts/apt-get.sh
+++ scripts/apt-get.sh
@@ -1,21 +1,40 @@
-# apt-get -y install git
-# apt-get -y install xclip
-# apt-get -y install curl
-apt-get update
-# Dramatis Personae
-apt-get -y install figlet #............ ASCII ART!!!!
-apt-get -y install graphviz #.......... visualization tool for ERDs
-apt-get -y install heroku-toolbelt #... Heroku command-line tooling for working with the Heroku platform
+# sudo apt-get -y install git
+# sudo apt-get -y install xclip
+# sudo apt-get -y install curl
+# sudo apt-get update
+# # Dramatis Personae
+# http://www.smashingmagazine.com/2011/06/21/set-up-an-ubuntu-local-development-machine-for-ruby-on-rails/
+# sudo apt-get install build-essential
+# sudo apt-get install bison
+# sudo apt-get install openssl
+# sudo apt-get install libreadline6
+# sudo apt-get install libreadline6-dev
+# sudo apt-get install zlib1g
+# sudo apt-get install zlib1g-dev
+# sudo apt-get install libssl-dev
+# sudo apt-get install libyaml-dev
+# sudo apt-get install libsqlite3-0
+# sudo apt-get install libsqlite3-dev
+# sudo apt-get install sqlite3
+# sudo apt-get install libxml2-dev
+# sudo apt-get install libxslt-dev
+# sudo apt-get install autoconf
+# sudo apt-get install libc6-dev
+# sudo apt-get install ncurses-dev
+
+sudo apt-get -y install figlet #............ ASCII ART!!!!
+sudo apt-get -y install graphviz #.......... visualization tool for ERDs
+sudo apt-get -y install heroku-toolbelt #... Heroku command-line tooling for working with the Heroku platform
 # http://eoinoc.net/install-hub-github-linux-mint/
-apt-get -y install imagemagick #....... image resizing
-apt-get -y install node #.............. Event-driven I/O server-side JavaScript environment based on V8
-apt-get -y install phantomjs #......... PhantomJS is a headless WebKit scriptable with a JavaScript API.
-apt-get -y install postgres #.......... open source object-relational database management system
-apt-get -y install qt #................ WebKit implementation of Qt
-apt-get -y install qt4 #............... qt for mavericks
+sudo apt-get -y install imagemagick #....... image resizing
+sudo apt-get -y install nodejs #.............. Event-driven I/O server-side JavaScript environment based on V8
+sudo apt-get -y install phantomjs #......... PhantomJS is a headless WebKit scriptable with a JavaScript API.
+sudo apt-get -y install postgres #.......... open source object-relational database management system
+sudo apt-get -y install qt #................ WebKit implementation of Qt
+sudo apt-get -y install qt4 #............... qt for mavericks
 # For Ruby
-apt-get -y install rbenv #............. our ruby version manager
-apt-get -y install ruby-build #........ provides an `rbenv install` command
+sudo apt-get -y install rbenv #............. our ruby version manager
+sudo apt-get -y install ruby-build #........ provides an `rbenv install` command
 
 # hub
 # http://eoinoc.net/install-hub-github-linux-mint/
@@ -24,22 +43,22 @@ curl http://defunkt.io/hub/standalone -Lo ~/bin/hub
 chmod 755 ~/bin/hub
 
 # # Additional Players
-# apt-get -y install rbenv-default-gems #  automatically install gems every time you install a new version of Ruby.
-# apt-get -y install autoconf #.......... Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.
-# apt-get -y install automake #.......... Automake is a tool for automatically generating Makefile.in
-# apt-get -y install git-flow #.......... Git extensions to provide high-level repository operations
-# apt-get -y install libtool #........... generic library support script
-# apt-get -y install libyaml #........... a YAML 1.1 parser and emitter
-# apt-get -y install neon #.............. neon is an HTTP and WebDAV client library
-# apt-get -y install openssl #........... A toolkit implementing SSL v2/v3 and TLS protocols with full-strength cryptography world-wide.
-# apt-get -y install pkg-config #........ pkg-config is a helper tool used when compiling applications and libraries.
-# apt-get -y install readline #.......... provides a set of functions for use by applications that allow users to edit command lines as they are typed in
-# apt-get -y install serf #.............. a high performance C-based HTTP client library
-# apt-get -y install sqlite #............ a self-contained, serverless, zero-configuration, transactional SQL database engine.
-# apt-get -y install ssh-copy-id #....... a script that uses ssh to log into a remote machine
-# apt-get -y install libxml2 #........... XML C parser and toolkit
-# apt-get -y install libxslt #........... a language for transforming XML documents into other XML documents.
-# apt-get -y install libiconv #.......... a conversion library between Unicode and traditional encoding
-# apt-get -y install ctags # ............  generates an index file of names found in source files of various programming languages.
+# sudo apt-get -y install rbenv-default-gems #  automatically install gems every time you install a new version of Ruby.
+# sudo apt-get -y install autoconf #.......... Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.
+# sudo apt-get -y install automake #.......... Automake is a tool for automatically generating Makefile.in
+# sudo apt-get -y install git-flow #.......... Git extensions to provide high-level repository operations
+# sudo apt-get -y install libtool #........... generic library support script
+# sudo apt-get -y install libyaml #........... a YAML 1.1 parser and emitter
+# sudo apt-get -y install neon #.............. neon is an HTTP and WebDAV client library
+# sudo apt-get -y install openssl #........... A toolkit implementing SSL v2/v3 and TLS protocols with full-strength cryptography world-wide.
+# sudo apt-get -y install pkg-config #........ pkg-config is a helper tool used when compiling applications and libraries.
+# sudo apt-get -y install readline #.......... provides a set of functions for use by applications that allow users to edit command lines as they are typed in
+# sudo apt-get -y install serf #.............. a high performance C-based HTTP client library
+# sudo apt-get -y install sqlite #............ a self-contained, serverless, zero-configuration, transactional SQL database engine.
+# sudo apt-get -y install ssh-copy-id #....... a script that uses ssh to log into a remote machine
+# sudo apt-get -y install libxml2 #........... XML C parser and toolkit
+# sudo apt-get -y install libxslt #........... a language for transforming XML documents into other XML documents.
+# sudo apt-get -y install libiconv #.......... a conversion library between Unicode and traditional encoding
+# sudo apt-get -y install ctags # ............  generates an index file of names found in source files of various programming languages.
 
 # fin
