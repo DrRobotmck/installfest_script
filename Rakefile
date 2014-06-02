a3628c1d54cccfb0ed536094c532154dd888a837
phlco <phillip.lamplugh@gmail.com>
2014-06-02 17:55:06 -0400

    adds default open for browser

---
+++ Rakefile
@@ -0,0 +1,11 @@
+task :default => 'build:ubuntu'
+
+namespace :build do
+
+  desc "ubuntu installfest script"
+  task :ubuntu do
+    puts 'build ubuntu tasks'
+    system("./bin/build.rb")
+  end
+
+end
--- install.sh
+++ install.sh
@@ -165,8 +165,9 @@ echo '- Click "Add key"'
 echo '- Confirm the action by entering your GitHub password'
 
 pause_awhile "Press Enter. We'll be here until you get back from Github."
-
-open https://github.com/settings/ssh
+echo "Firefox may print an error to the terminal. DON'T WORRY!"
+# Open in default browser as a new process
+xdg-open https://github.com/settings/ssh &
 
 pause_awhile "Ok. Ready to Continue? Press Enter."
 #
