d05a0fc5ee30d84b91133211b824c003695366e5
phlco <phillip.lamplugh@gmail.com>
2014-06-02 22:05:30 -0400

    adds ubuntu

---
+++ lib/ubuntu/apps.sh
@@ -0,0 +1,20 @@
+# Google Chrome.........The Browser
+# HipChat...............The Chat Client
+# Sublime Text 2........The Text Editor
+
+# chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+# hipchat
+echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
+wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+apt-get update
+apt-get install hipchat
+
+# sublime
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
---
+++ lib/ubuntu/hub.sh
@@ -0,0 +1,4 @@
+# http://eoinoc.net/install-hub-github-linux-mint/
+mkdir ~/bin
+curl http://defunkt.io/hub/standalone -Lo ~/bin/hub
+chmod 755 ~/bin/hub
