b17fe6bcf8bb8278dc3bd07c75629e9828c3cb48
phlco <phillip.lamplugh@gmail.com>
2014-06-03 13:54:49 -0400

    incorportes colors

--- lib/colors.sh
+++ lib/colors.sh
@@ -1,33 +1,31 @@
-#-------------------------------------------------------------------------------
-# Set colors
-#-------------------------------------------------------------------------------
-# http://en.wikipedia.org/wiki/Tput
+# Reset formatting
+RESET=$(      tput sgr0)
 
-# Foreground
-BLACK=$(tput setaf 0)
-BLUE=$(tput setaf 4)
-CYAN=$(tput setaf 6)
-GREEN=$(tput setaf 2)
-MAGENTA=$(tput setaf 5)
-ORANGE=$(tput setaf 172)
-PURPLE=$(tput setaf 141)
-RED=$(tput setaf 1)
-WHITE=$(tput setaf 7)
-YELLOW=$(tput setaf 226)
+# Foreground color
+BLACK=$(      tput setaf 0)
+RED=$(        tput setaf 1)
+GREEN=$(      tput setaf 2)
+YELLOW=$(     tput setaf 3)
+BLUE=$(       tput setaf 4)
+MAGENTA=$(    tput setaf 5)
+CYAN=$(       tput setaf 6)
+WHITE=$(      tput setaf 9)
+# ORANGE=$(     tput setaf 172)
+# PURPLE=$(     tput setaf 141)
 
-# Background
-BG_BLACK=$(tput setab 0)
-BG_BLUE=$(tput setab 4)
-BG_CYAN=$(tput setab 6)
-BG_GREEN=$(tput setab 2)
-BG_MAGENTA=$(tput setab 5)
-BG_ORANGE=$(tput setab 172)
-BG_RED=$(tput setab 1)
-BG_WHITE=$(tput setab 7)
-BG_YELLOW=$(tput setab 226)
+# Background color
+BG_BLACK=$(   tput setab 0)
+BG_RED=$(     tput setab 1)
+BG_GREEN=$(   tput setab 2)
+BG_YELLOW=$(  tput setab 3)
+BG_BLUE=$(    tput setab 4)
+BG_MAGENTA=$( tput setab 5)
+BG_CYAN=$(    tput setab 6)
+BG_WHITE=$(   tput setab 9)
+# BG_ORANGE=$(  tput setab 172)
 
-# Formatting
-UNDERLINE=$(tput smul)
-NOUNDERLINE=$(tput rmul)
-BOLD=$(tput bold)
-RESET=$(tput sgr0)
+# Style
+UNDERLINE=$(  tput smul)
+# NOUNDERLINE=$(tput rmul)
+BOLD=$(       tput bold)
+ITALIC=$(     tput sitm)
