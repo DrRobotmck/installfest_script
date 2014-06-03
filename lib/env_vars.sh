5bbc8971495c5da7b96d9930d3b0333fee20e84f
phlco <phillip.lamplugh@gmail.com>
2014-06-03 00:33:07 -0400

    testing merge

--- lib/env_vars.sh
+++ lib/env_vars.sh
@@ -1,6 +1,6 @@
-BRANCH="v2"
 BELOVED_RUBY_VERSION="1.9.3-p484"
 
+# TODO backport this into master and mac...
 SRC_DIR=~/.wdi/installfest
 SETTINGS=$SRC_DIR/settings
 INSTALL_REPO=https://github.com/phlco/installfest_scripts.git
