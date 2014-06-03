a542ac3be1181ebf7aae79f26561975bf4bce17a
phlco <phillip.lamplugh@gmail.com>
2014-06-02 20:42:40 -0400

    working through install

--- scripts/apt-get.sh
+++ scripts/apt-get.sh
@@ -4,34 +4,74 @@
 # sudo apt-get update
 # # Dramatis Personae
 # http://www.smashingmagazine.com/2011/06/21/set-up-an-ubuntu-local-development-machine-for-ruby-on-rails/
-# sudo apt-get install build-essential
-# sudo apt-get install bison
-# sudo apt-get install openssl
-# sudo apt-get install libreadline6
-# sudo apt-get install libreadline6-dev
-# sudo apt-get install zlib1g
-# sudo apt-get install zlib1g-dev
-# sudo apt-get install libssl-dev
-# sudo apt-get install libyaml-dev
-# sudo apt-get install libsqlite3-0
-# sudo apt-get install libsqlite3-dev
-# sudo apt-get install sqlite3
-# sudo apt-get install libxml2-dev
-# sudo apt-get install libxslt-dev
-# sudo apt-get install autoconf
-# sudo apt-get install libc6-dev
-# sudo apt-get install ncurses-dev
+
+# autoconf
+# automake
+# bison
+# build-essential
+# curl
+# exuberant-ctags
+# git-core
+# git-doc
+# imagemagick
+# libc6-dev
+# libnotify-bin
+# libpq-dev
+# libreadline6
+# libreadline6-dev
+# libsqlite3-0
+# libsqlite3-dev
+# libssl-dev
+# libtool
+# libxml2-dev
+# libxslt-dev
+# libyaml-dev
+# make
+# mercurial
+# ncurses-dev
+# ncurses-term
+# openjdk-6-jdk
+# openssl
+# pgadmin3
+# postgresql
+# postgresql-contrib
+# ruby
+# ruby-dev
+# sqlite3
+# ssh
+# subversion
+# vim
+# vim-gnome
+# vim-rails
+# zlib1g
+# zlib1g-dev
 
 sudo apt-get -y install figlet #............ ASCII ART!!!!
 sudo apt-get -y install graphviz #.......... visualization tool for ERDs
-sudo apt-get -y install heroku-toolbelt #... Heroku command-line tooling for working with the Heroku platform
+
+# Heroku command-line tooling for working with the Heroku platform
 # http://eoinoc.net/install-hub-github-linux-mint/
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 sudo apt-get -y install imagemagick #....... image resizing
 sudo apt-get -y install nodejs #.............. Event-driven I/O server-side JavaScript environment based on V8
+sudo apt-get -y install npm
+# For programs that still depend on calling the "node" binary, run:
+sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install phantomjs #......... PhantomJS is a headless WebKit scriptable with a JavaScript API.
-sudo apt-get -y install postgres #.......... open source object-relational database management system
-sudo apt-get -y install qt #................ WebKit implementation of Qt
-sudo apt-get -y install qt4 #............... qt for mavericks
+
+# open source object-relational database management system
+# https://www.digitalocean.com/community/articles/how-to-install-and-use-postgresql-on-ubuntu-12-04
+# TODO phlco set up user
+# https://help.ubuntu.com/community/PostgreSQL
+# sudo apt-get -y install postgresql-xc-client
+# sudo apt-get -y install postgresql-client
+# sudo apt-get -y install postgresql
+# sudo apt-get -y install postgresql-contrib
+# sudo apt-get -y install pg_ctl # initialize stop/start posgres
+
+# FIXME sudo apt-get -y install qt #................ WebKit implementation of Qt
+# FIXME sudo apt-get -y install qt4 #............... qt for mavericks
 # For Ruby
 sudo apt-get -y install rbenv #............. our ruby version manager
 sudo apt-get -y install ruby-build #........ provides an `rbenv install` command
@@ -44,8 +84,9 @@ chmod 755 ~/bin/hub
 
 # # Additional Players
 # sudo apt-get -y install rbenv-default-gems #  automatically install gems every time you install a new version of Ruby.
-# sudo apt-get -y install autoconf #.......... Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.
+sudo apt-get -y install autoconf #.......... Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.
 # sudo apt-get -y install automake #.......... Automake is a tool for automatically generating Makefile.in
+sudo apt-get -y install bison #.............. general-purpose parser generator that converts an annotated context-free grammar into an LALR or GLR parser for that grammar.
 # sudo apt-get -y install git-flow #.......... Git extensions to provide high-level repository operations
 # sudo apt-get -y install libtool #........... generic library support script
 # sudo apt-get -y install libyaml #........... a YAML 1.1 parser and emitter
