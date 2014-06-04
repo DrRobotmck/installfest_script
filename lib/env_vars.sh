12d21c014677914772b8e2e98b68a17fac225d0f
h4w5 <philip@h4w5.com>
2014-06-04 17:40:04 -0400

    realized branch env var is a problem

--- lib/env_vars.sh
+++ lib/env_vars.sh
@@ -1,8 +1,7 @@
+# BRANCH=
+
 BELOVED_RUBY_VERSION="Ruby 1.9.3-p547"
 CURRENT_STABLE_RUBY_VERSION="Ruby 2.1.2"
-# current branch name
-# git rev-parse --abbrev-ref HEAD
-BRANCH="ubuntu-merge"
 
 # TODO backport this into master and mac...
 SRC_DIR=~/.wdi/installfest
