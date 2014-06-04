4ddf70f5315889edf113ca9e4c6316b74cf86956
h4w5 <philip@h4w5.com>
2014-06-03 21:35:53 -0400

    fixed install repo from refactoring

--- lib/env_vars.sh
+++ lib/env_vars.sh
@@ -3,4 +3,4 @@ BELOVED_RUBY_VERSION="1.9.3-p484"
 # TODO backport this into master and mac...
 SRC_DIR=~/.wdi/installfest
 SETTINGS=$SRC_DIR/settings
-INSTALL_REPO=https://github.com/phlco/installfest_scripts.git
+INSTALL_REPO=https://github.com/ga-instructors/installfest_script.git
