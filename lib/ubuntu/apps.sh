4133cca76f5d89048a4802d5b1ce94fb8f0917df
h4w5 <philip@h4w5.com>
2014-06-04 16:36:40 -0400

    moved atom out of apps to take snapshot

--- lib/ubuntu/apps.sh
+++ lib/ubuntu/apps.sh
@@ -34,20 +34,3 @@ chmod 755 ~/usr/bin/hub
 
 # vim
 sudo apt-get -y install vim
-
-# atom
-# Requires 64-bit
-# ensure that gyp uses Python 2
-sudo npm config set python /usr/bin/python2 -g
-git clone https://github.com/atom/atom
-cd atom
-# TMPDIR=~/.wdi/tmp
-# Creates application at $TMPDIR/atom-build/Atom
-# TODO (phlco) move to /usr/bin
-script/build
-# Installs command to /usr/local/bin/atom
-sudo script/grunt install
-# Generates a .deb package at $TMPDIR/atom-build
-script/grunt mkdeb
-# TODO (phlco) command line tool for atom?
-# sudo dpkg -i $TMPDIR/atom-build/Atom.deb
