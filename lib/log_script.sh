bfc2c3a2447a28a12b4ddd2490a5b7ac40b11e90
h4w5 <philip@h4w5.com>
2014-06-03 20:49:36 -0400

    step back -- testing log function

--- lib/log_script.sh
+++ lib/log_script.sh
@@ -1,8 +1,8 @@
-# http://stackoverflow.com/questions/3173131/redirect-copy-of-stdout-to-log-file-from-within-bash-script-itself
 
-# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
+mkdir ~/.wdi # must make this here in order to store the log file
 exec > >(tee ~/.wdi/install.log)
 
+# http://stackoverflow.com/questions/3173131/redirect-copy-of-stdout-to-log-file-from-within-bash-script-itself
 # Without this, only stdout would be captured - i.e. your
 # log file would not contain any error messages.
 # SEE answer by Adam Spiers, which keeps STDERR a seperate stream -
