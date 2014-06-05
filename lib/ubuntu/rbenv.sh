cd74726a793751f5ee5c9c4eacb26f92306fc7eb
phlco <phillip.lamplugh@gmail.com>
2014-06-05 00:15:31 -0400

    removes ruby from version name

--- lib/ubuntu/rbenv.sh
+++ lib/ubuntu/rbenv.sh
@@ -21,9 +21,9 @@ git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-source ~/.bashrc
-
 export PATH="$HOME/.rbenv/bin:$PATH"'
 eval "$(rbenv init -)"'
+
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# source ~/.bashrc
