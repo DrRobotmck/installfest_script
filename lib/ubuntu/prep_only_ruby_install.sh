99677578d156e063f89d5693df5ba0d8737e3b3f
h4w5 <philip@h4w5.com>
2014-06-05 17:32:58 -0400

    added necessary changes to have a ruby only script

---
+++ lib/ubuntu/prep_only_ruby_install.sh
@@ -0,0 +1,9 @@
+
+# this is a subset of the install_system_packages script for the purpose of
+# setting up an ubuntu-rubyonly install
+
+# OpenSSL implementation of SSL
+sudo apt-get -y install libssl-dev
+
+# git for version control
+sudo apt-get -y install git
