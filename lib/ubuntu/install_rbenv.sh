a76f020a154f4df58055a6e3b9a9f8986b1cd759
h4w5 <philip@h4w5.com>
2014-06-05 15:33:29 -0400

    big renaming

---
+++ lib/ubuntu/install_rbenv.sh
@@ -0,0 +1,25 @@
+# # our ruby version manager
+# sudo apt-get -y install rbenv
+# # git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+# # provides an `rbenv install` command
+# sudo apt-get -y install ruby-build
+#
+# # ruby_check=$(rbenv versions | grep $BELOVED_RUBY_VERSION)
+# #
+# # if [[ "$ruby_check" == *$BELOVED_RUBY_VERSION* ]]; then
+# #   echo "$BELOVED_RUBY_VERSION is installed"
+# # else
+# #   rbenv install $BELOVED_RUBY_VERSION
+# # fi
+#
+#
+# # TODO (h4w5) must come to understand this...
+#
+# source $HOME/.bashrc
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+
+export PATH="$HOME/.rbenv/bin:$PATH"'
+eval "$(rbenv init -)"'
+
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
---
+++ lib/ubuntu/install_system_packages.sh
@@ -0,0 +1,59 @@
+# Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.
+sudo apt-get -y install autoconf
+
+# Automake is a tool for automatically generating Makefile.in
+sudo apt-get -y install automake
+
+# general-purpose parser generator that converts an annotated context-free grammar into an LALR or GLR parser for that grammar.
+sudo apt-get -y install bison
+
+# generic library support script
+sudo apt-get -y install libtool
+
+# a YAML 1.1 parser and emitter
+sudo apt-get -y install libyaml
+
+# provides a set of functions for use by applications that allow users to edit command lines as they are typed in
+sudo apt-get -y install libreadline6 libreadline6-dev
+
+# XML C parser and toolkit
+sudo apt-get -y install libxml2-dev
+
+# a language for transforming XML documents into other XML documents.
+sudo apt-get -y install libxslt1-dev
+
+# gnome-keyring is a daemon in the session, similar to ssh-agent,
+# and other applications can use it to store passwords and other
+# sensitive information
+# atom dependency?
+sudo apt-get -y install libgnome-keyring-dev
+
+# OpenSSL implementation of SSL
+sudo apt-get -y install libssl-dev
+
+# git for version control
+sudo apt-get -y install git
+
+# copy to clipboard
+sudo apt-get -y install xclip
+
+# generates an index file of names found in source files of various programming languages.
+sudo apt-get -y install exuberant-ctags
+
+# ASCII ART!!!!
+sudo apt-get -y install figlet
+
+# visualization tool for ERDs
+sudo apt-get -y install graphviz
+
+# image resizing
+sudo apt-get -y install imagemagick
+
+# PhantomJS is a headless WebKit scriptable with a JavaScript API.
+sudo apt-get -y install phantomjs
+
+# a self-contained, serverless, zero-configuration, transactional SQL database engine.
+sudo apt-get -y install sqlite3
+
+# extras
+sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
