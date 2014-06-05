377eeae1248a6f5f9f18cf71d962d11a54c216d0
h4w5 <philip@h4w5.com>
2014-06-05 10:55:06 -0400

    change ruby version in env vars to try gem

--- lib/env_vars.sh
+++ lib/env_vars.sh
@@ -1,9 +1,7 @@
-BELOVED_RUBY_VERSION="Ruby 1.9.3-p547"
+BELOVED_RUBY_VERSION="Ruby 1.9.3-rc1"
 CURRENT_STABLE_RUBY_VERSION="Ruby 2.1.2"
 
 # TODO backport this into master and mac...
 SRC_DIR=~/.wdi/installfest
 SETTINGS=$SRC_DIR/settings
 INSTALL_REPO=https://github.com/ga-instructors/installfest_script.git
-
-# moving branch to a file dynamically built from rake
