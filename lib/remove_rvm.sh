12f1fc3d858ca3202654fc6bff04b6d16af251da
h4w5 <philip@h4w5.com>
2014-06-07 10:02:45 -0400

    institute some assertions -- not tested yet, rollback if necessary

--- lib/remove_rvm.sh
+++ lib/remove_rvm.sh
@@ -1,23 +1,8 @@
-# Because we're going to use rbenv and homebrew we need to remove RVM and MacPorts
-# This script checks for and removes previous installs of macports and RVM
-
-# Uninstall RVM
+# uninstall rvm, so that we can use rbenv
 # http://stackoverflow.com/questions/3950260/howto-uninstall-rvm
 if hash rvm 2>/dev/null || [ -d ~/.rvm ]; then
   yes | rvm implode
   rm -rf ~/.rvm
-  echo "RVM has been removed."
-else
-  echo "RVM is not installed. Moving on..."
 fi
 
-# # http://blog.coolaj86.com/articles/installing-ruby-on-ubuntu-12-04.html
-# sudo apt-get remove --purge ruby-rvm ruby
-# sudo rm -rf /usr/share/ruby-rvm /etc/rmvrc /etc/profile.d/rvm.sh
-#
-# # See if there is anything
-# ls -d ~/.rvm*
-# ls -d ~/.gem*
-# ls -d ~/.bundle*
-# # Delete them (ignoring those that don't exist)
-# rm -rf ~/.rvm* ~/.gem/ ~/.bundle*
+assert_that "RVM has been removed or was not installed" "command -v rvm" ""
--- lib/ubuntu/prep_only_ruby_install.sh
+++ lib/ubuntu/prep_only_ruby_install.sh
@@ -4,6 +4,8 @@
 
 # OpenSSL implementation of SSL
 sudo apt-get -y install libssl-dev
+assert_that "OpenSSL implementation downloaded for git" "apt-cache show libssl-dev"
 
 # git for version control
 sudo apt-get -y install git
+assert_that "OpenSSL implementation downloaded for git" "apt-cache show git"
