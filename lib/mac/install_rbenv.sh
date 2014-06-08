f40fdd9d36ff5fa9f11942095cce45a99d5eb684
phlco <phillip.lamplugh@gmail.com>
2014-06-08 12:27:39 -0400

    switching rbenv to brew on mac

--- lib/mac/install_rbenv.sh
+++ lib/mac/install_rbenv.sh
@@ -1,14 +1,6 @@
 # Our Ruby version manager
-# brew install rbenv
- git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-
-# Automatically runs rbenv rehash every time you install or uninstall a gem.
-# brew install rbenv-gem-rehash
-git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-
-# Provides an `rbenv install` command
-# brew install ruby-build
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+brew install rbenv
+# git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
 # Add to path
 export PATH="$HOME/.rbenv/bin:$PATH"
@@ -16,6 +8,20 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 # enable shims and autocompletion
 eval "$(rbenv init -)"
 
+# Automatically runs rbenv rehash every time you install or uninstall a gem.
+brew install rbenv-gem-rehash
+# git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
 # Automatically install gems every time you install a new version of Ruby
-# brew install rbenv-default-gems
-git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+brew install rbenv-default-gems
+# git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+
+# automatically runs rbenv rehash every time you install or uninstall a gem
+brew install rbenv-gem-rehash
+# git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+# Provides an `rbenv install` command
+brew install ruby-build
+# git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+# 
+#
