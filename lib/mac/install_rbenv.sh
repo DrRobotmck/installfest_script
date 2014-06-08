4c4f20ec5281e05a7e87c747b7aad5c570251c17
phlco <phillip.lamplugh@gmail.com>
2014-06-08 14:26:04 -0400

    adds rbenv_root var

--- lib/mac/install_rbenv.sh
+++ lib/mac/install_rbenv.sh
@@ -2,13 +2,29 @@
 brew install rbenv
 # git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
+# Use homebrew's directories instead of rbenv's
+# echo 'export RBENV_ROOT=/usr/local/var/rbenv' >> ~/.bash_profile
+export RBENV_ROOT=/usr/local/var/rbenv
+
 # Add to path
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' > ~/.bash_profile
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+export PATH="$HOME/.rbenv/bin:$PATH"
 
 # enable shims and autocompletion
-echo 'eval "$(rbenv init -)"' > ~/.bash_profile
+# echo '# added by installfest script' >> ~/.bash_profile
+# echo 'if which rbenv > /dev/null; then eval "$(rbenv init-)": fi' >> ~/.bash_profile
+if which rbenv > /dev/null; then eval "$(rbenv init-)": fi
 
 # Automatically runs rbenv rehash every time you install or uninstall a gem.
+
+# -If the `GEM_PATH` environment variable is undefined, rbenv-gem-rehash
+# -must first execute the `gem env gempath` command to retrieve RubyGems'
+# -default path so that it can can *append* to the path rather than
+# -override it. This can take a while—from a few hundred milliseconds on
+# -MRI to several seconds on JRuby—so the default path for the current
+# -Ruby version is cached to the filesystem the first time it is
+# -retrieved.
+
 brew install rbenv-gem-rehash
 # git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
@@ -16,14 +32,11 @@ brew install rbenv-gem-rehash
 brew install rbenv-default-gems
 # git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
 
-# automatically runs rbenv rehash every time you install or uninstall a gem
-brew install rbenv-gem-rehash
-# git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-
 # Provides an `rbenv install` command
-brew install ruby-build
+## ruby-build is a dependency of rbenv-default-gems, so it gets installed
+# brew install ruby-build
 # git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-rbenv rehash
+# rbenv rehash
 
-source ~/.bash_profile
+# source ~/.bash_profile
