b79dda491143a0901f1b1e268acd8f4ea114328f
phlco <phillip.lamplugh@gmail.com>
2014-06-08 20:34:26 -0400

    cleaning up mac rbenv

--- lib/mac/install_postgres.sh
+++ lib/mac/install_postgres.sh
@@ -10,8 +10,9 @@ createdb ${USER}
 # Ensure that Postgres launches whenever we login
 
 # # http://robots.thoughtbot.com/starting-and-stopping-background-services-with-homebrew
-brew services start postgres
-# mkdir -p ~/Library/LaunchAgents
-# cp /usr/local/Cellar/postgresql/9.*/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
-# # Start Postgres now
-# launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+# FIXME (phlco) brew way doesn't work
+# brew services start postgres
+mkdir -p ~/Library/LaunchAgents
+cp /usr/local/Cellar/postgresql/9.*/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
+# Start Postgres now
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
--- lib/mac/install_rbenv.sh
+++ lib/mac/install_rbenv.sh
@@ -1,43 +1,34 @@
 # Our Ruby version manager
-brew install rbenv
-# git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+# brew install rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
 # Use homebrew's directories instead of rbenv's
 # echo 'export RBENV_ROOT=/usr/local/var/rbenv' >> ~/.bash_profile
 # export RBENV_ROOT="/usr/local/var/rbenv"
 
-# Add to path
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'if which rbenv > /dev/null; then eval "$(rbenv init-)"; fi' >> ~/.bash_profile
-source  ~/.bash_profile
+# # Add to path
+# echo '# added by installfest script' >> ~/.bash_profile
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile
+# source  ~/.bash_profile
 
 # enable shims and autocompletion
-# echo '# added by installfest script' >> ~/.bash_profile
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
 # Automatically runs rbenv rehash every time you install or uninstall a gem.
 
-# -If the `GEM_PATH` environment variable is undefined, rbenv-gem-rehash
-# -must first execute the `gem env gempath` command to retrieve RubyGems'
-# -default path so that it can can *append* to the path rather than
-# -override it. This can take a while—from a few hundred milliseconds on
-# -MRI to several seconds on JRuby—so the default path for the current
-# -Ruby version is cached to the filesystem the first time it is
-# -retrieved.
-
-brew install rbenv-gem-rehash
-# git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+# brew install rbenv-gem-rehash
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
 # Automatically install gems every time you install a new version of Ruby
-brew install rbenv-default-gems
-# git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+# brew install rbenv-default-gems
+git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
 
-# Provides an `rbenv install` command
-## ruby-build is a dependency of rbenv-default-gems, so it gets installed
-brew install ruby-build
-# git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+# # Provides an `rbenv install` command
+# # ruby-build is a dependency of rbenv-default-gems, so it gets installed
+# brew install ruby-build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-rbenv rehash
-
+# rbenv rehash
 # source ~/.bash_profile
