37946c54df6dc5bd6d0131934b380a0c36c7f01d
phlco <phillip.lamplugh@gmail.com>
2014-06-09 00:28:06 -0400

    fixing postgres user

--- lib/mac/install_postgres.sh
+++ lib/mac/install_postgres.sh
@@ -1,18 +1,17 @@
 # Set up Postgres
 
-# open source object-relational database management system
-brew install postgres
-
 # Create a database
 initdb /usr/local/var/postgres -E utf8
-createdb ${USER}
 
 # Ensure that Postgres launches whenever we login
-
-# # http://robots.thoughtbot.com/starting-and-stopping-background-services-with-homebrew
-# FIXME (phlco) brew way doesn't work
-# brew services start postgres
 mkdir -p ~/Library/LaunchAgents
 cp /usr/local/Cellar/postgresql/9.*/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
+
 # Start Postgres now
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+
+# give postgres time to load
+sleep 5s
+
+# create db matching user name so we can log in by just typing psql
+createdb ${USER}
