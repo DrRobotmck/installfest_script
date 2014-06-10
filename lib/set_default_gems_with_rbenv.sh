c4acd01d226348a0817a7968b722f4c1c0cb760e
h4w5 <philip@h4w5.com>
2014-06-10 09:15:36 -0400

    switch default gems to a list and loop

--- lib/set_default_gems_with_rbenv.sh
+++ lib/set_default_gems_with_rbenv.sh
@@ -1,52 +1,29 @@
+
 # Our gems to install
 # skip documentation
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 
-# TODO (phlco) move these to a default gem file that gets copied over
 # TODO (phlco) replace ~/.rbenv with $RBENV_ROOT
 touch ~/.rbenv/default-gems
 
-# Maintains a consistent environment for ruby applications.
-echo "bundler" >> ~/.rbenv/default-gems
+gemlist=(
+  bundler         # Maintains a consistent environment for ruby applications.
+  capybara        # Acceptance test framework for web applications
+  guard           # handle events on file system modifications
+  jasmine         # JavaScript testing
+  pg              # ruby interface for Postgres
+  pry             # alternative to the standard IRB shell
+  pry-coolline    # live syntax highlighting for the Pry REPL
+  rails           # full stack, Web application framework
+  rspec           # testing tool for Ruby
+  sinatra         # a DSL for quickly creating web applications in Ruby
+  sinatra-contrib # common Sinatra extensions
+  github_api      # Ruby interface to github API v3
+  hipchat         # HipChat HTTP API Wrapper
+  awesome_print   # pretty print your Ruby objects with style
+  rainbow         # colorizing printed text on ANSI terminals
+)
 
-# Acceptance test framework for web applications
-echo "capybara" >> ~/.rbenv/default-gems
-
-# handle events on file system modifications
-echo "guard" >> ~/.rbenv/default-gems
-
-# JavaScript testing
-echo "jasmine" >> ~/.rbenv/default-gems
-
-# ruby interface for Postgres
-echo "pg" >> ~/.rbenv/default-gems
-
-# alternative to the standard IRB shell
-echo "pry" >> ~/.rbenv/default-gems
-
-# live syntax highlighting for the Pry REPL
-echo "pry-coolline" >> ~/.rbenv/default-gems
-
-# full stack, Web application framework
-echo "rails" >> ~/.rbenv/default-gems
-
-# testing tool for Ruby
-echo "rspec" >> ~/.rbenv/default-gems
-
-# a DSL for quickly creating web applications in Ruby
-echo "sinatra" >> ~/.rbenv/default-gems
-
-# common Sinatra extensions
-echo "sinatra-contrib" >> ~/.rbenv/default-gems
-
-# Ruby interface to github API v3
-echo "github_api" >> ~/.rbenv/default-gems
-
-# HipChat HTTP API Wrapper
-echo "hipchat" >> ~/.rbenv/default-gems
-
-# pretty print your Ruby objects with style
-echo "awesome_print" >> ~/.rbenv/default-gems
-
-# colorizing printed text on ANSI terminals
-echo "rainbow" >> ~/.rbenv/default-gems
+for gem in ${gemlist[@]}; do
+  echo "${gem}" >> ~/.rbenv/default-gems
+done
