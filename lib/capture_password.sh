ae8531e2e00035c419b3d43ccecadbbed3a7aacc
h4w5 <philip@h4w5.com>
2014-06-10 15:21:20 -0400

    update capture_pass with new prompt and -v flag in keep-alive

--- lib/capture_password.sh
+++ lib/capture_password.sh
@@ -1,7 +1,5 @@
 # Capture the user's password
-sudo echo "Thanks."
+sudo -p "Please enter your computer's password so that we can make the necessary changes: " echo "Thanks."
 
 # Update existing `sudo` time stamp until script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# TODO (phlco) does this actually work?
+while true; do sudo -v true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
