b08dba4591581b5ae0a52bf6ba2e17162d078b55
phlco <phillip.lamplugh@gmail.com>
2014-06-27 09:59:11 -0400

    rebuilt scripts

--- lib/capture_password.sh
+++ lib/capture_password.sh
@@ -1,2 +1,3 @@
 # capture the user's password
-sudo -p "Enter your computer's password so that we can make the necessary changes: " echo "Thank you."
+sudo -p "Enter your computer's password so that we can make the necessary changes: "
+echo "Thank you."
