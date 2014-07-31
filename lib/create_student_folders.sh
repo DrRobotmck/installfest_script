c4cd6d32182207f7ed4d81045aa6df64dbfed6aa
phlco <phillip.lamplugh@gmail.com>
2014-07-31 13:55:08 -0400

    adds mac address

--- lib/create_student_folders.sh
+++ lib/create_student_folders.sh
@@ -1,2 +1,4 @@
+echo "Creating a student folder in ~/dev/wdi"
+
 STUDENT_FOLDER="$HOME/dev/wdi"
 mkdir -p $STUDENT_FOLDER
---
+++ lib/install_git_completion.sh
@@ -0,0 +1,3 @@
+git_completion_url=https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+curl -o ~/.git-completion.bash $git_completion_url
