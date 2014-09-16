2bb092c4eb3b94fd5bcede19a12a50c9a0c9bb15
amadden80 <amadden80@gmail.com>
2014-09-16 15:39:26 -0400

    gitname included in email message

--- lib/report_log.sh
+++ lib/report_log.sh
@@ -1,11 +1,12 @@
 
 # email subject
-SUBJECT="Log Report"
+SUBJECT="Installfest Log Report for $github_name $github_email"
 # Email To ?
 EMAIL="andrew@ga.co"
 # Email text/message
 EMAILMESSAGE="/tmp/emailmessage.txt"
 cat "$HOME/.wdi/install.log"> $EMAILMESSAGE
+
 # send an email using /bin/mail
 mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
 
