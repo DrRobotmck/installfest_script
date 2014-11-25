c622ecfa36ae3a00b1acb189e76498f90b00b937
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-11-25 10:01:42 -0500

    moves manifests and builds into folders

--- bin/build.rb
+++ bin/build.rb
@@ -3,18 +3,16 @@
 # TODO(phlco) allow arguments?
 # build = ARGV[0]
 
-Dir["Manifest*"].each do |manifest|
+Dir["manifests/Manifest*"].each do |manifest|
   filename = manifest.gsub(/manifest./i, '')
-  File.open(filename, 'w') do |file|
+  File.open("builds/#{filename}", 'w') do |file|
     File.read(manifest).each_line do |line|
       unless line.start_with?("#") || line.strip == ""
         file_path = (line =~ /^tests\// ? "#{line.chomp}.sh" : "lib/#{line.chomp}.sh")
         file << File.read(file_path)
         file << "\n"
       else
-        if line.start_with?('# ->')
-          file << "\n\n#{line.gsub('# ->', '').strip}\n\n"
-        end
+        file << "\n\n#{line.gsub('# ->', '').strip}\n\n" if line.start_with?('# ->')
       end
     end
   end
