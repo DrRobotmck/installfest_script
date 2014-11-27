242ff7fcaf5d988aee2af2e1fe4046bfea957c4c
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2014-11-26 23:14:05 -0500

    adds check to download repo vs clone

--- lib/capture_password.sh
+++ lib/capture_password.sh
@@ -1,2 +1,2 @@
 # capture the user's password
-sudo -p "${GREEN}Enter your computer's password so that we can make the necessary changes: ${RESET}" echo "Thank you."
+sudo -p "${BG_GREEN}Enter your computer's password so that we can make the necessary changes: ${RESET}" echo "Thank you."
